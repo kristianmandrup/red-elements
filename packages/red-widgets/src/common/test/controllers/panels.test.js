8c987ac2e9a116b59eb259bcef1c80b61fe56f08
Kristian Mandrup <kmandrup@gmail.com>
2017-11-15 06:50:57 +0700

    more common widget tests

--- packages/red-widgets/src/common/test/controllers/panels.test.js
+++ packages/red-widgets/src/common/test/controllers/panels.test.js
@@ -1,4 +1,7 @@
 import {
+  readPage,
+  ctx,
+  RED,
   controllers
 } from '../imports'
 
@@ -8,8 +11,52 @@ const {
 
 const clazz = Panel
 
+const {
+  log
+} = console
+
+beforeAll(() => {
+  // Panel has no widget factory, just a class
+
+  // load document with placeholder elements to create widgets (for testing)
+  document.documentElement.innerHTML = readPage('simple')
+})
+
 describe('Panel', () => {
   test('is a class', () => {
     expect(typeof clazz).toBe('function')
   })
+})
+
+function makePanel(opts) {
+  try {
+    return widgetElem = new Panel({
+      id: 'invalid-panel-1',
+    })
+    true
+  } catch (err) {
+    return {
+      invalid: true,
+      err
+    }
+  }
+}
+
+
+test('Panel: can NOT be created from id unless has 2 child elements', () => {
+  let elems = ['invalid-panel-1', 'invalid-panel-2'].map(id => {
+    return makePanel({
+      id
+    })
+  })
+  elems.map(elem => expect(elem.invalid).toBe(true))
+})
+
+
+test('Panel: can be created from id when has 2 child elements', () => {
+  let widgetElem = new Panel({
+    id: 'valid-panel',
+  })
+
+  expect(widgetElem).toBeDefined()
 })
--- packages/red-widgets/src/common/test/controllers/popover.test.js
+++ packages/red-widgets/src/common/test/controllers/popover.test.js
@@ -1,4 +1,7 @@
 import {
+  readPage,
+  ctx,
+  RED,
   controllers
 } from '../imports'
 
@@ -8,6 +11,25 @@ const {
 
 const clazz = Popover
 
+const {
+  log
+} = console
+
+beforeAll(() => {
+  // Popover has no widget factory, just a class
+
+  // load document with placeholder elements to create widgets (for testing)
+  document.documentElement.innerHTML = readPage('simple')
+})
+
 test('Popover: is a class', () => {
   expect(typeof clazz).toBe('function')
+})
+
+test('Popover: widget can be created from id', () => {
+  let widgetElem = new Popover({
+    id: 'popover',
+  })
+
+  expect(widgetElem).toBeDefined()
 })
