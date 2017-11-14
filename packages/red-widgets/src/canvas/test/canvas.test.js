2a25c099ee5db9682373f2720892f15d9fe1bada
Kristian Mandrup <kmandrup@gmail.com>
2017-11-15 06:16:55 +0700

    fix canvas instance vars

--- packages/red-widgets/src/canvas/test/canvas.test.js
+++ packages/red-widgets/src/canvas/test/canvas.test.js
@@ -1,13 +1,22 @@
 import {
-  test,
-  nightmare,
-  View
-} from '../imports'
-const ctx = {}
+  View,
+  RED,
+  readPage,
+  ctx
+} from './imports'
 
 function create(ctx) {
   return new View(ctx)
 }
+
+let view
+beforeEach(() => {
+  view = create(ctx)
+})
+
+beforeAll(() => {
+  document.documentElement.innerHTML = readPage('simple')
+})
 
 test('View: create', () => {
   let diff = create(ctx)
