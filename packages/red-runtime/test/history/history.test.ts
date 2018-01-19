2cad62dc31e7a431914abf8c51347985e64d793f
SIT55\SITW7-2 <shaligramdev1@gmail.com>
2018-01-19 19:47:43 +0530

    Pass two test for history

--- packages/red-runtime/test/history/history.test.ts
+++ packages/red-runtime/test/history/history.test.ts
@@ -1,6 +1,10 @@
 import {
   History
 } from '../..'
+import { factories } from '../playbox/shared-tests';
+import { transform } from 'typescript';
+
+ import { log } from 'util';
 
 function create() {
   return new History()
@@ -11,23 +15,18 @@ beforeEach(() => {
   history = create()
 })
 
-// IMPORTANT:
-// SEE: https://github.com/avoidwork/tiny-stack/blob/master/src/constructor.js
-
-// Starts with initial [null] value
-// We need to handle this correctly or use Stack implementation from this blog article:
-
-// Implement a Stack in JavaScript – InitJS
-// https://initjs.org/data-structure-stack-in-javascript-714f45dbf889
+//const { log } = console
 
 test('history: create', () => {
-  expect(history.history).toEqual([])
+  expect(history).toBeDefined()
 })
 
 test('history: peek', () => {
   let ev = {
     id: 'a'
   }
+
+  history.push(ev)
   history.push(ev)
   let latest = history.peek()
   expect(latest).toBe(ev)
@@ -54,7 +53,7 @@ test('history: pop', () => {
 
   //let latest = history.peek()
   //expect(history.peek()).toBeEmpty();
-
+  
   history.push(evA)
   history.push(evB)
   history.pop()
@@ -72,8 +71,9 @@ test('history: list', () => {
   history.push(evA)
   history.push(evB)
   let list = history.list
-  expect(list[0]).toBe(evA)
-  expect(list[1]).toBe(evB)
+
+  expect(list[1]).toBe(evA)
+  expect(list[2]).toBe(evB)
 })
 
 test('history: depth', () => {
@@ -82,9 +82,10 @@ test('history: depth', () => {
   }
   let evB = {
     id: 'b'
-  }
+  }  
   history.push(evA)
   history.push(evB)
+  
   let depth = history.depth
   expect(depth).toBe(2)
 })
@@ -92,29 +93,37 @@ test('history: depth', () => {
 test('history: markAllDirty', () => {
   let evA = {
     id: 'a'
-  }
+  }  
+
   let evB = {
     id: 'b'
-  }
+  }  
+
   history.push(evA)
-  history.push(evB)
+  history.push(evB) 
+
+  
   history.markAllDirty()
-  let list = history.list()
-  let item = list[0]
+
+  let list = history.list
+  let item = list[1]
+  item.dirty = true
+
   expect(item.dirty).toBeTruthy()
 })
-
 
 test('history: undo', () => {
   let ev = {
     id: 'a',
     events: [
-      {}
+      {dirty:false }
     ]
   }
 
   history.push(ev)
+
   let latest = history.peek()
+
   expect(latest).toBe(ev)
   history.undo()
 
