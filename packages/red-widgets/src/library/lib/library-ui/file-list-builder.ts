e93d2ede4231dad045ebae16dbe89ad8b5102be8
Kristian Mandrup <kmandrup@gmail.com>
2018-01-21 12:12:05 +0700

    better FileListBuilder or library

--- packages/red-widgets/src/library/lib/library-ui/file-list-builder.ts
+++ packages/red-widgets/src/library/lib/library-ui/file-list-builder.ts
@@ -1,7 +1,26 @@
+import {
+  Context,
+  $
+} from '../../../context'
+
+import { LibraryUI } from './';
+import { LibraryApi } from '@tecla5/red-runtime/src/api/library-api';
+import { dirname } from 'path';
+
 /**
- *
+ * Build File List of libraries from server via API call
  */
-export class FileListBuilder {
+export class FileListBuilder extends Context {
+  protected libraryApi: LibraryApi
+
+  constructor(public ui: LibraryUI) {
+    super()
+  }
+
+  /**
+   * Build a library file list item in the UI for the library file structure
+   * @param item
+   */
   buildFileListItem(item) {
     var li = document.createElement("li");
     li.onmouseover = function (e) {
@@ -13,65 +32,164 @@ export class FileListBuilder {
     return li;
   }
 
+  isDirectory(v) {
+    return typeof v === 'string'
+  }
+
+  isFile(v) {
+    return !this.isDirectory
+  }
+
   /**
-   * TODO: extract Ajax calls to use generic API
+   * Build file/directory structure of libraries available on server
    *
-   * @param root
-   * @param data
+   * @param root { string } root folder
+   * @param data { Array<Object> } library structure
    */
   buildFileList(root: string, data: any[]) {
+    const {
+      isDirectory,
+      buildFileListItem,
+      createDirectoryItemClickHandler,
+      createFileItemClickHandler
+    } = this.rebind([
+        'isDirectory',
+        'buildFileListItem',
+        'createDirectoryItemClickHandler',
+        'createFileItemClickHandler'
+      ])
+
     var ul = document.createElement("ul");
     var li;
     for (var i = 0; i < data.length; i++) {
       var v = data[i];
-      if (typeof v === "string") {
-        // directory
-        li = this.buildFileListItem(v);
-        li.onclick = (() => {
-          var dirName = v;
-          return (e) => {
-            var bcli = $('<li class="active"><span class="divider">/</span> <a href="#">' + dirName + '</a></li>');
-            $("a", bcli).click((e) => {
-              $(this).parent().nextAll().remove();
 
-              $.getJSON("library/" + this.options.url + root + dirName, (data) => {
-                $("#node-select-library").children().first().replaceWith(this.buildFileList(root + dirName + "/", data));
-              });
+      // directory
+      if (isDirectory(v)) {
+        li = buildFileListItem(v);
+        li.onclick = createDirectoryItemClickHandler(v, root)
 
-              e.stopPropagation();
-            });
-            var bc = $("#node-dialog-library-breadcrumbs");
-            $(".active", bc).removeClass("active");
-            bc.append(bcli);
-
-            $.getJSON("library/" + this.options.url + root + dirName, (data) => {
-              $("#node-select-library").children().first().replaceWith(this.buildFileList(root + dirName + "/", data));
-            });
-
-          }
-        })();
         li.innerHTML = '<i class="fa fa-folder"></i> ' + v + "</i>";
         ul.appendChild(li);
       } else {
         // file
-        li = this.buildFileListItem(v);
+        li = buildFileListItem(v);
         li.innerHTML = v.name;
-        li.onclick = (() => {
-          var item = v;
-          return (e) => {
-            $(".list-selected", ul).removeClass("list-selected");
-            $(this).addClass("list-selected");
-
-            $.get("library/" + this.options.url + root + item.fn, (data) => {
-              this.selectedLibraryItem = item;
-              this.libraryEditor.setValue(data, -1);
-            });
-
-          }
-        })();
+        li.onclick = createFileItemClickHandler(v, root, ul)
         ul.appendChild(li);
       }
     }
     return ul;
+  }
+
+  /**
+   * Create click handler to load file item from Api
+   * @param v
+   * @param root
+   * @param ul
+   */
+  createFileItemClickHandler(v, root, ul) {
+    const {
+      loadLibraryFileItem
+    } = this.rebind([
+        'loadLibraryFileItem'
+      ])
+
+    return () => {
+      var item = v;
+      return function (e) {
+        $(".list-selected", ul).removeClass("list-selected");
+        $(this).addClass("list-selected");
+
+        const url = 'library/' + this.options.url + root + item.fn
+        loadLibraryFileItem(item, url)
+      }
+    }
+  }
+
+  set selectedLibraryItem(item) {
+    this.ui.selectedLibraryItem = item
+  }
+
+  get libraryEditor() {
+    return this.ui.libraryEditor
+  }
+
+  async loadLibraryFileItem(item, url) {
+    const {
+      libraryApi,
+      onLibraryItemLoadSuccess,
+      onLibraryItemLoadError,
+    } = this
+    try {
+      const result = await libraryApi.load({
+        url
+      })
+      onLibraryItemLoadSuccess(result, item)
+    } catch (error) {
+      onLibraryItemLoadError(error)
+    }
+  }
+
+  onLibraryItemLoadSuccess(data, item) {
+    let {
+      selectedLibraryItem,
+      libraryEditor
+    } = this
+    selectedLibraryItem = item;
+    libraryEditor.setValue(data, -1);
+  }
+
+  // TODO
+  onLibraryItemLoadError(error) {
+  }
+
+  createDirectoryItemClickHandler(v, root) {
+    const {
+      loadDirectoryItem
+    } = this.rebind([
+        'loadDirectoryItem'
+      ])
+
+    var dirName = v;
+    return function (e) {
+      var bcli = $('<li class="active"><span class="divider">/</span> <a href="#">' + dirName + '</a></li>');
+      $("a", bcli).click(function (e) {
+        $(this).parent().nextAll().remove();
+
+        loadDirectoryItem(this.options, root, dirName)
+        e.stopPropagation();
+      });
+      var bc = $("#node-dialog-library-breadcrumbs");
+      $(".active", bc).removeClass("active");
+      bc.append(bcli);
+
+      loadDirectoryItem(this.options, root, dirName)
+    }
+  }
+
+  async loadDirectoryItem(options, root, dirName) {
+    const url = 'library/' + options.url + root + dirName
+    const {
+      libraryApi,
+      onDirectoryItemLoadSuccess,
+      onDirectoryItemLoadError,
+    } = this
+    try {
+      const result = await libraryApi.load({
+        url
+      })
+      onDirectoryItemLoadSuccess(result, root, dirname)
+    } catch (error) {
+      onDirectoryItemLoadError(error)
+    }
+  }
+
+  onDirectoryItemLoadSuccess(data, root, dirName) {
+    $("#node-select-library").children().first().replaceWith(this.buildFileList(root + dirName + "/", data));
+  }
+
+  onDirectoryItemLoadError(error) {
+
   }
 }
